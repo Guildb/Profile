93c048f3358bab23eb196e019e8fc4a8b8e8429e
guildb <5silvr53@solent.ac.uk>
2024-07-08 16:48:26 +0100

    applicaiton finished

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -39,9 +39,7 @@ const EducationExperience = () => {
 
   return (
     <div
-      className="text-center my-8 px-4"
-      data-aos="flip-up"
-      data-aos-delay="300"
+      className=" my-8 px-4"
     >
       <h2 className="text-3xl font-bold mb-8">Experience</h2>
       <VerticalTimeline>
